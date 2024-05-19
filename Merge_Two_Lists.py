688823dcdcab88d89da5b5edddd79edf8fb7419a
sinistergeek <susanbhattraidon@gmail.com>
2024-05-19 10:06:35 +0545

    Merge_Two_Lists

--- Merge_Two_Lists.py
+++ Merge_Two_Lists.py
@@ -3,3 +3,11 @@ vegetables = ['potato','brinjal','cauliflower']
 
 fruits_vegetables = fruits + vegetables
 print(fruits_vegetables)
+
+
+#Merge Two list of different Types 
+
+list1 = ['apple','orange','banana']
+list2 = [5,8,10]
+merged_list = list1 + list2
+print(merged_list)
